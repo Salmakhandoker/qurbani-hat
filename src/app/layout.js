220d1907516c61fe26cc9a3e6bae6ecc51e40a09
Salmakhandoker <salmakhandoker001@gmail.com>
2026-06-30 14:17:50 +0600

    feat: add global react-hot-toast Toaster component to RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ dns.setServers(["8.8.8.8","8.8.4.4"]);
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 export const metadata = {
   title: "QurbaniHat",
@@ -12,8 +13,9 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="emerald">
       <body className="bg-gray-50 text-gray-800">
+        <Toaster position="top-center" reverseOrder={false} />
 
         {/* Navbar */}
         <Navbar />
